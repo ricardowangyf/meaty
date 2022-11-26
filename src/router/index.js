8948583ff0d49f0afb457397ded49308976da809
logicwang <1758555877@qq.com>
2022-11-26 21:53:07 +0800

    添加路由

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import NotFound from '../view/NotFound.vue'
+import HomePage from '../view/HomePage.vue'
+
+
+export default new Router({
+    mode: 'history',
+    linkActiveClass: 'isactive',
+    routes: [
+        {
+            path: '/',
+            redirect: '/HomePage',
+            component: HomePage,
+            children: [
+                // {
+                //     path: '/list/:type',
+                //     component: Items,
+                //     props: true
+                // },
+            ]
+        },
+        {
+            path: '*',
+            name: '404',
+            component: NotFound
+        },
+    ]
+})
+const originalPush = Router.prototype.push
+
+Router.prototype.push = function push(location) {
+    return originalPush.call(this, location).catch(err => err)
+}
+Vue.use(Router);
