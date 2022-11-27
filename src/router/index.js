fa7a2061afca84b9147cf91f058854013380c6a4
logicwang <1758555877@qq.com>
2022-11-27 23:59:57 +0800

    修改路由大小写

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ export default new Router({
     routes: [
         {
             path: '/',
-            redirect: '/HomePage',
+            redirect: '/homepage',
             component: HomePage,
             children: [
                 // {
