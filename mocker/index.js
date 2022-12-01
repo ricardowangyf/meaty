8c27a2286bb688f91b2c942df95dceb3670563c1
logicwang <1758555877@qq.com>
2022-12-01 12:07:29 +0800

    添加数据

--- mocker/index.js
+++ mocker/index.js
@@ -9,10 +9,17 @@ const proxy = {
     'GET /api/user/': [
         {
             "name": "仙人掌",
-            "favorties": true,
-            "trash": "2022/12/01",
             "imgurl": "/img/cactus.png",
             "paragraph":"叶片一般都是很饱满的，并且株型也很紧凑，橘珠看起来是很小巧的...",
+            "favorties": true,
+            "trash": "2022/12/01",
+        },
+        {
+            "name": "多肉植物",
+            "imgurl": "/img/Succulentplants.jpg",
+            "paragraph":"叶片一般都是很饱满的，并且株型也很紧凑，橘珠看起来是很小巧的...",
+            "favorties": true,
+            "trash": "2022/12/01",
         },
     ],
 }
