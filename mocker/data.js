f8cc37abc60382bc97a39ab6d219169318c84dde
logicwang <1758555877@qq.com>
2022-12-06 01:00:43 +0800

    修改模拟数据

---
+++ mocker/data.js
@@ -0,0 +1,25 @@
+exports.list = [
+    {
+        "name": "仙人掌",
+        "imgurl": "/img/cactus.jpg",
+        "paragraph":"叶片一般都是很饱满的,并且株型也很紧凑,橘珠看起来x是很小巧的...",
+        "favorties": true,
+        "trash": "2022/12/01",
+    },
+    {
+        "name": "多肉植物",
+        "imgurl": "/img/Succulentplants.jpg",
+        "paragraph":"多肉植物也叫多水植物、肉质植物,是指植物器官的茎或叶或根具有发达的薄壁组织用以贮藏水分,在外形上显得肥厚多汁的一类植物。",
+        "favorties": true,
+        "trash": "2022/12/01",
+    },
+    {
+        "name": "多肉植物2",
+        "imgurl": "/img/Succulentplants.jpg",
+        "paragraph":"叶片一般都是很饱满的,并且株型也很紧凑,橘珠看起来是很小巧的...",
+        "favorties": true,
+        "trash": "2022/12/01",
+    },
+  ]
+  
+  
---
+++ mocker/detalis.js
@@ -0,0 +1,14 @@
+const delay = require('mocker-api/lib/delay');
+const noProxy = process.env.NO_PROXY === 'true';
+const { list }= require('./data')
+
+const proxy = {
+  'POST /api/user/list/details': (req, res) => {
+    const  name  = req.body.name;
+    const items = name && list.filter(item => (item.name === name));
+    const data = items && items.length > 0 ? items[0] : {}
+    return res.json(data);
+  },
+}
+
+module.exports = proxy, noProxy, delay;
