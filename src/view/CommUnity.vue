c78f44c7db29e370d8a2bca6665edc6968cdfbec
logicwang <1758555877@qq.com>
2022-12-24 11:03:53 +0800

    添加底部导航栏

--- src/view/CommUnity.vue
+++ src/view/CommUnity.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
     xxxxasdadsasdwfwedfgwaregegrx
+    <FooterPage />
   </div>
 </template>
 
 <script>
-export default {};
+import FooterPage from '../components/Footer.vue'
+export default {
+  name:"CommUnity",
+  components: {
+    FooterPage
+  },
+};
 </script>
 
 <style>
+li {
+  list-style-type: none;
+}
+a {
+  text-decoration: none;
+}
 </style>
