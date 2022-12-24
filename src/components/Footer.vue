dd1b9ba1dec5f4b1c001fb84318232bf4ce898a6
logicwang <1758555877@qq.com>
2022-12-24 09:53:01 +0800

    添加底部导航和过滤按钮组件

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,44 +1,42 @@
 <template>
-  <div>
-    <div class="navigationbar">
-      <router-link to="/all" style="background: white">
-        <div class="padding">
-          <li>
-            <img src="../assets/img/home.svg" />
-          </li>
-          <li class="home">主页</li>
-        </div>
-      </router-link>
-      <router-link to="/commUnity" style="background: white">
-        <div class="padding">
-          <li>
-            <img src="../assets/img/community.svg" />
-          </li>
-          <li class="home">社区</li>
-        </div>
-      </router-link>
-      <router-link to="/shopcenter" style="background: white">
-        <div class="padding">
-          <li>
-            <img src="../assets/img/shop.svg" />
-          </li>
-          <li class="home">商城</li>
-        </div>
-      </router-link>
-    </div>
+  <div class="navigationbar">
+    <router-link to="/all" class="backgroundd">
+      <div class="padding">
+        <li>
+          <img src="../assets/img/home.svg" />
+        </li>
+        <li class="home">主页</li>
+      </div>
+    </router-link>
+    <router-link to="/commUnity"  class="backgroundd">
+      <div class="padding">
+        <li>
+          <img src="../assets/img/community.svg" />
+        </li>
+        <li class="home">社区</li>
+      </div>
+    </router-link>
+    <router-link to="/shopcenter"  class="backgroundd">
+      <div class="padding">
+        <li>
+          <img src="../assets/img/shop.svg" />
+        </li>
+        <li class="home">商城</li>
+      </div>
+    </router-link>
   </div>
 </template>
 <script>
-export default{
-  name:'FooterPage',
-}
+export default {
+  name: "FooterPage",
+};
 </script>
 <style lang='less'>
 .navigationbar {
   position: fixed;
   background: white;
   top: 666px;
-  width: 22.4rem;
+  width: 100%;
   box-shadow: 0 -3px 6px 0 rgb(82 84 91 / 13%),
     0 3px 6px 0 rgb(106 108 118 / 13%);
   display: flex;
@@ -53,6 +51,9 @@ export default{
     color: #929292;
     letter-spacing: 0.04px;
     text-align: center;
+  }
+  .backgroundd{
+    background: white
   }
 }
 </style>
