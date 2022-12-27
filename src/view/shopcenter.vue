56f925559f57fd27262385088a6db4307451b800
logicwang <1758555877@qq.com>
2022-12-27 20:35:27 +0800

    添加按钮加入购物车

--- src/view/shopcenter.vue
+++ src/view/shopcenter.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="juzhong">
     <Filterbutton />
-    <div >
+    <div>
       <li
         v-for="(tableData, i) in tableData"
         :key="tableData.name"
@@ -18,8 +18,11 @@
           :eal="tableData.paragraph"
         />
       </li>
+      <div class="gouwucjhe">
+        <h1 class="gouwuche">加入购物车</h1>
+      </div>
     </div>
-    <FooterPage/>
+    <FooterPage />
   </div>
 </template>
 <script>
@@ -67,21 +70,41 @@ a {
   padding-top: 6px;
   opacity: 0.6;
 }
+.paragraphcontent {
+  display: flex;
+  justify-content: flex-start;
+  background: #fff;
+  border: none;
+}
+.juzhong {
+  margin-left: 25px;
+  margin-right: 25px;
+}
 .background {
   background: #ffffff;
-  margin-bottom: 2rem;
+  margin-bottom: 1.9rem;
   box-shadow: 0 -3px 6px 0 rgb(82 84 91 / 13%),
     0 3px 6px 0 rgb(106 108 118 / 13%);
   border-radius: 5.04px;
   width: 100%;
   height: 9rem;
 }
-.paragraphcontent {
-  display: flex;
-  justify-content: flex-start;
-}
-.juzhong {
-  margin-left: 26px;
-  margin-right: 31px;
+
+.gouwucjhe {
+  background: #76c477;
+    position: relative;
+    bottom: 76px;
+    width: 78px;
+    left: 239px;
+    border-top-left-radius: 22px;
+    height: 22px;
+  .gouwuche {
+    margin: 0;
+    font-family: Helvetica;
+    font-size: 5.1px;
+    color: #ffffff;
+    letter-spacing: 0;
+    text-align: right;
+  }
 }
 </style>
