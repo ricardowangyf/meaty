ce2c5f4dfbd1f0890ab30a29a634b0367c8e001b
logicwang <1758555877@qq.com>
2023-01-16 09:36:29 +0800

    change devServer secure bull

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     proxy: {
       '/api': {
         target: 'http://localhost:3721', // 这里可以跟随项目实际部署服务器来
-        secure: false, // 如果是https接口，需要配置这个参数
+        secure: true, // 如果是https接口，需要配置这个参数
         changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
       },
     }
