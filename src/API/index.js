285841bab9eba574e15c8575030201fefc1e0b50
logicwang <1758555877@qq.com>
2023-01-17 18:41:11 +0800

    commit all

--- src/API/index.js
+++ src/API/index.js
@@ -1,5 +1,5 @@
 import requests from './request'
 
-export const reqCategoryList = () => requests({ url: '/api/user/', method: 'GET' }) //中间路由
+export const reqCategoryList = () => requests({ url: 'http://localhost:3721/api/user/', method: 'GET' }) //中间路由
 
-export const meatydetali = () => requests({ url: '/api/user/list', method: 'GET' }) //中间路由
+export const meatydetali = () => requests({ url: 'http://localhost:3721/api/user/list', method: 'GET' }) //中间路由
