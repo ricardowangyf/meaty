cd05510bba1ac9aeae4063396ad5579dac12fd7b
Ricardowang <1758555877@qq.com>
2023-02-08 23:21:02 +0800

    add js calculate font size

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,23 @@ export default {
   // components: {
   //   HomePage,
   // },
+  mounted() {
+      let width=("body");
+      if(width>=1200){
+        let fontsize=width/1920*40;//fontsize为当前屏幕的基数字体，相对于设计稿计算得到的。
+        ("html").css("font-size",`${fontsize}px`)
+      }//当加载页面的时候设置生效
+      window.onresize = () => {//当页面尺寸改变的时候生效
+        return (() => {
+          let width=("body");
+          if(width>=1200){
+            let fontsize=width/1920*40;
+            ("html").css("font-size",`${fontsize}px`)
+          }
+        })()
+      }
+    }
+
 };
 </script>
 
